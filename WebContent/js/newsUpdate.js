4ff9a8395547a4932e55ae766925bb2acf9ee9f4
Veronica.C <cyj0318@gmail.com>
2016-09-11 09:31:51 +0800

    got my location

--- WebContent/js/newsUpdate.js
+++ WebContent/js/newsUpdate.js
@@ -1,12 +1,10 @@
-/* javaScript for newsUpdate*/
-
 function getNewsupdate(){	
 	//append html
 	var contenDiv ="<div data-role='collapsible'>";
 	var contenDivEnd ="</div>";	
 	var fullDivContent;
 	$.ajax({
-		async : false, //需同步處理嗎,同步處理，jquery的格式才會出來
+		async : false, //同步處理，jquery的格式才會出來
 		type : "POST",
 		url : "http://127.0.0.1:8080/DrinkApp/getNews",
 		success : function(data){
@@ -35,20 +33,18 @@ function getNewsupdate(){
 /*below is function for welcome page to resize the height/width according to window*/
 $(document).ready(function(){
 	$(window).resize(function(){
+		//shop resized
 		$("#welcomePage").css('width',$(window).width());
 		$("#welcomePage").css('height',$(window).height());
 		$("#welcomImgDiv").css('width',$(window).width());
 		$("#welcomImgDiv").css('height',$(window).height());
-		
 		$("#shop").css('width',$(window).width());
 		$("#shop").css('height',$(window).height());
-		$("#divFmap").css('width',$(window).width());
-		$("#divFmap").css('height','80%');
 		
-		$("#googleMap").css('width','100%');
-		$("#googleMap").css('height','100%');
+		//map resized
+		$("#divFmap").css('width',$(window).width());
+		$("#divFmap").css('height','80%');	
+		
+		
 	})
 })
-		
-		
-		
--- WebContent/js/shop.js
+++ WebContent/js/shop.js
@@ -1,7 +1,3 @@
-
-
-/* google map setting */
-
 $(document).on("pagecreate","#shop",function(event){
 	var startNorth;
 	var startEast;
@@ -13,205 +9,270 @@ $(document).on("pagecreate","#shop",function(event){
 	var shop3 = new google.maps.LatLng(24.165928, 120.631680);
 	var shop4 = new google.maps.LatLng(24.139124, 120.680541);
 	var shop5 = new google.maps.LatLng(24.135787, 120.610822);
-	var nowPosition;	
+//	var nowPosition;
+	var haveUserLocation = false;
 
 	//info index
 	var shopAddress = document.getElementById("address");
 	var shopTel = document.getElementById("tel");
 	var shopOpen = document.getElementById("openHoure");
-
-	var mapProp = {
-		center : myCenter,
-		zoom : 13,
-		mapTypeId : google.maps.MapTypeId.ROADMAP
-	};
-
-	var map = new google.maps.Map($("#googleMap")[0], mapProp);	
 	
-
-
-//set marker
- 	var marker= new google.maps.Marker({
-	  	position:myCenter,
-	  	// animation:google.maps.Animation.BOUNCE //jump
-	  	title:"總店",
- 	})
-  	var marker2= new google.maps.Marker({
-  		position:shop2,
-  		title:"澄清店",
- 	})
-
-  	var marker3= new google.maps.Marker({
-  		position:shop3,
-  		title:"環河店",
-  	})
-  	var marker4= new google.maps.Marker({
-  		position:shop4,
-  		title:"民族店",
-  	})
-
-  	var marker5= new google.maps.Marker({
-  		position:shop5,
-  		title:"嶺東店",
-  	})
-
-  	marker.setMap(map);
-  	marker2.setMap(map);
-  	marker3.setMap(map);
-  	marker4.setMap(map);
-  	marker5.setMap(map);	  	
-
-  	var infowindow = new google.maps.InfoWindow({
-  		content:"總店 <br/> 地址：台中市西屯區惠來路二段 <br/> 電話：04-6666-8888"
-  	});
-  	// infowindow.open(map,marker);
-
-  	var infowindow = new google.maps.InfoWindow({
-  		content:"澄清店 <br/> 地址：台灣台中市西屯區台灣大道四段966號 <br/> 電話：04-6666-7777"
-  	});
-  	// infowindow.open(map,marker2);
-
-  	var infowindow = new google.maps.InfoWindow({
-  		content:"環河店 <br/> 地址：台中市西屯區台中市西屯區市政北二路400號 <br/> 電話：04-6666-6666"
-  	});
-  	// infowindow.open(map,marker3);
-
-  	var infowindow = new google.maps.InfoWindow({
-  		content:"民族店 <br/> 地址：台灣台中市中區民族路67號 <br/> 電話：04-6666-5555"
-  	});
-  	// infowindow.open(map,marker4);
-
-  	var infowindow = new google.maps.InfoWindow({
-  		content:"嶺東店 <br/> 地址：台灣台中市南屯區建功路50號 <br/> 電話：04-6666-3333"
-  	});
-
- // marker click event
-  	google.maps.event.addListener(marker, 'click', function() {
-  		if (locations.length>1){locations =[];} //clear array when triple click
-  		getLocation();//get user location
-  		locations.push(marker); //push marker in locations[1];
-        DrawRoutePath();//root organize
-
-        //show shop info
-        shopAddress.innerHTML = "地址：台中市西屯區惠來路二段";
-		shopTel.innerHTML = "電話：04-6666-8888";
-		shopOpen.innerHTML = "營業時間：0900-2100";
-	  	infowindow.open(map,marker);
-	});
-
-  	google.maps.event.addListener(marker2, 'click', function() {
-  		if (locations.length>1){locations =[];} //clear array when triple click
-	  	getLocation();//get user location
-	  	locations.push(marker2); //push marker in locations[1];
-        DrawRoutePath();//root organize
-
-        //show shop info
-        shopAddress.innerHTML = "地址：台灣台中市西屯區台灣大道四段966號";
-		shopTel.innerHTML = "電話：04-6666-7777";
-		shopOpen.innerHTML = "營業時間：0900-2100";
-
-	    infowindow.open(map,marker2);
-	});
-
-	google.maps.event.addListener(marker3, 'click', function() {
-		if (locations.length>1){locations =[];} //clear array when triple click
-		getLocation();//get user location
-		locations.push(marker3); //push marker in locations[1];
-        DrawRoutePath();//root organize
-
-        //show shop info
-        shopAddress.innerHTML = "地址：台中市西屯區台中市西屯區市政北二路400號";
-		shopTel.innerHTML = "電話：04-6666-6666";
-		shopOpen.innerHTML = "營業時間：0900-2100";
-
-	    infowindow.open(map,marker3);
-	});
-
-	google.maps.event.addListener(marker4, 'click', function() {
-		if (locations.length>1){locations =[];} //clear array when triple click
-		getLocation();//get user location
-		locations.push(marker4); //push marker in locations[1];
-        DrawRoutePath();//root organize
-//show shop info
-        shopAddress.innerHTML = "地址：台灣台中市中區民族路67號";
-		shopTel.innerHTML = "電話：04-6666-5555";
-		shopOpen.innerHTML = "營業時間：0900-2100";
-	  	infowindow.open(map,marker4);
-	});
-
-	google.maps.event.addListener(marker5, 'click', function() {
-		if (locations.length>1){locations =[];} //clear array when triple click
-		getLocation();//get user location
-		locations.push(marker5); //push marker in locations[1];
-        DrawRoutePath();//root organize
-//show shop info
-        shopAddress.innerHTML = "地址：台灣台中市南屯區建功路50號";
-		shopTel.innerHTML = "電話：04-6666-3333";
-		shopOpen.innerHTML = "營業時間：0900-2100";
-		infowindow.open(map,marker5);
-	});
+	var map;
+	
+	//get location and set map center
+	
+	function getCurrentLocation() {
+		if (navigator.geolocation) {//有抓到使用者位置，以使用者為中心
+			navigator.geolocation.getCurrentPosition(showCurrentPosition);
+		} else {                    //沒抓到使用者位置，以總店為中心
+			divLocation.innerHTML = "Geolocation is not supported.";
+			noNowPosition();
+		}
+	}
+	
+	function showCurrentPosition(position) {
+		haveUserLocation =true;
+		var hint = document.getElementById("mapInfo"); //display mapInfo
+		hint.innerHTML = "現在位置：("+position.coords.latitude+","+position.coords.longitude+")";
+		var mylocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+			
+		var userMaker= new google.maps.Marker({
+	  		position:mylocation,
+	  		animation:google.maps.Animation.BOUNCE
+	  	})
 		
-//get user geolocation
-	//  var startNorth;
-	// 	var startEast;
-	//  1'st to last  (緯度Latitude(startNorth),經度Longitude(startEast))
-	var x = document.getElementById("mapInfo"); //display mapInfo
-
-	function getLocation() {
-	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(showPosition, showError);
-	    } else {
-	        x.innerHTML = "Geolocation is not supported by this browser.";
-	    }
+		var mapProp = {
+			center : mylocation,
+			zoom : 9,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map($("#googleMap")[0], mapProp);
+		userMaker.setMap(map);
+		locations.push(userMaker);//push marker, get .position
+		nextStep();
 	}
-
-	function showPosition(position) {
-		// if (locations.length>1){locations =[];} //clear array when triple click
-		startNorth = position.coords.latitude;
-		startEast = position.coords.longitude;
-		nowPosition = new google.maps.LatLng(startNorth, startEast);
-//		locations.push(nowPosition);//put user position in [0]
-		x.innerHTML = "("+startNorth+","+startEast+")";
-	}
-
-	function showError(error) {
-	    switch(error.code) {
-	        case error.PERMISSION_DENIED:
-	            x.innerHTML = "User denied the request for Geolocation."
-	            break;
-	        case error.POSITION_UNAVAILABLE:
-	            x.innerHTML = "Location information is unavailable."
-	            break;
-	        case error.TIMEOUT:
-	            x.innerHTML = "The request to get user location timed out."
-	            break;
-	        case error.UNKNOWN_ERROR:
-	            x.innerHTML = "An unknown error occurred."
-	            break;
-	    }
-	}
-    
-    //root path service
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(map);
-    
-    function DrawRoutePath() {
-        if (locations.length < 2)
-            return;
-        var start = locations[0].position;
-        var end = locations[1].position
-        var request = {
-            origin : start,
-            destination : end,
-            travelMode : google.maps.TravelMode.DRIVING
-        };
-        directionsService.route(request, function(response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                directionsDisplay.setDirections(response);
-            }
-        });
-    }  // end of DrawRoutePath()
-});
 	
-		  
+	function noNowPosition(){
+		var mapProp = {
+			center : myCenter,
+			zoom : 13,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};	
+		map = new google.maps.Map($("#googleMap")[0], mapProp);	
+		nextStep();
+	}
+
+	getCurrentLocation(); //啟動取得使用者未置
+	
+
+	function nextStep(){
+//set marker
+	 	var marker= new google.maps.Marker({
+		  	position:myCenter,
+		  	// animation:google.maps.Animation.BOUNCE //jump
+		  	title:"總店",
+	 	})
+	  	var marker2= new google.maps.Marker({
+	  		position:shop2,
+	  		title:"澄清店",
+	 	})
+	
+	  	var marker3= new google.maps.Marker({
+	  		position:shop3,
+	  		title:"環河店",
+	  	})
+	  	var marker4= new google.maps.Marker({
+	  		position:shop4,
+	  		title:"民族店",
+	  	})
+	
+	  	var marker5= new google.maps.Marker({
+	  		position:shop5,
+	  		title:"嶺東店",
+	  	})
+	 	
+	  	marker.setMap(map);
+	  	marker2.setMap(map);
+	  	marker3.setMap(map);
+	  	marker4.setMap(map);
+	  	marker5.setMap(map);	  	
+	
+	  	var infowindow = new google.maps.InfoWindow({
+	  		content:"總店 <br/> 地址：台中市西屯區惠來路二段 <br/> 電話：04-6666-8888"
+	  	});
+	  	// infowindow.open(map,marker);
+	
+	  	var infowindow = new google.maps.InfoWindow({
+	  		content:"澄清店 <br/> 地址：台灣台中市西屯區台灣大道四段966號 <br/> 電話：04-6666-7777"
+	  	});
+	  	// infowindow.open(map,marker2);
+	
+	  	var infowindow = new google.maps.InfoWindow({
+	  		content:"環河店 <br/> 地址：台中市西屯區台中市西屯區市政北二路400號 <br/> 電話：04-6666-6666"
+	  	});
+	  	// infowindow.open(map,marker3);
+	
+	  	var infowindow = new google.maps.InfoWindow({
+	  		content:"民族店 <br/> 地址：台灣台中市中區民族路67號 <br/> 電話：04-6666-5555"
+	  	});
+	  	// infowindow.open(map,marker4);
+	
+	  	var infowindow = new google.maps.InfoWindow({
+	  		content:"嶺東店 <br/> 地址：台灣台中市南屯區建功路50號 <br/> 電話：04-6666-3333"
+	  	});
+	
+	 // marker click event
+	  	google.maps.event.addListener(marker, 'click', function() {
+	  		if(haveUserLocation){
+		  		if (locations.length>1){ //如果location[]有兩個元素，丟掉最後一個(第一個是使用者位置)
+		  			locations.pop();
+		  		} 
+	  		}else{
+	  			if (locations.length>1){locations =[];}
+	  		}
+	//  		getLocation();//get user location
+	  		locations.push(marker); //push marker in locations[1];
+	        DrawRoutePath();//root organize
+	
+	        //show shop info
+	        shopAddress.innerHTML = "地址：台中市西屯區惠來路二段";
+			shopTel.innerHTML = "電話：04-6666-8888";
+			shopOpen.innerHTML = "營業時間：0900-2100";
+		  	infowindow.open(map,marker);
+		});
+	
+	  	google.maps.event.addListener(marker2, 'click', function() {
+	  		if(haveUserLocation){
+		  		if (locations.length>1){ //如果location[]有兩個元素，丟掉最後一個(第一個是使用者位置)
+		  			locations.pop();
+		  		} 
+	  		}else{
+	  			if (locations.length>1){locations =[];}
+	  		}	//	  	getLocation();//get user location
+		  	locations.push(marker2); //push marker in locations[1];
+	        DrawRoutePath();//root organize
+	
+	        //show shop info
+	        shopAddress.innerHTML = "地址：台灣台中市西屯區台灣大道四段966號";
+			shopTel.innerHTML = "電話：04-6666-7777";
+			shopOpen.innerHTML = "營業時間：0900-2100";
+	
+		    infowindow.open(map,marker2);
+		});
+	
+		google.maps.event.addListener(marker3, 'click', function() {
+			if(haveUserLocation){
+		  		if (locations.length>1){ //如果location[]有兩個元素，丟掉最後一個(第一個是使用者位置)
+		  			locations.pop();
+		  		} 
+	  		}else{
+	  			if (locations.length>1){locations =[];}
+	  		}	//		getLocation();//get user location
+			locations.push(marker3); //push marker in locations[1];
+	        DrawRoutePath();//root organize
+	
+	        //show shop info
+	        shopAddress.innerHTML = "地址：台中市西屯區台中市西屯區市政北二路400號";
+			shopTel.innerHTML = "電話：04-6666-6666";
+			shopOpen.innerHTML = "營業時間：0900-2100";
+	
+		    infowindow.open(map,marker3);
+		});
+	
+		google.maps.event.addListener(marker4, 'click', function() {
+			if(haveUserLocation){
+		  		if (locations.length>1){ //如果location[]有兩個元素，丟掉最後一個(第一個是使用者位置)
+		  			locations.pop();
+		  		} 
+	  		}else{
+	  			if (locations.length>1){locations =[];}
+	  		}	//		getLocation();//get user location
+			locations.push(marker4); //push marker in locations[1];
+	        DrawRoutePath();//root organize
+	//show shop info
+	        shopAddress.innerHTML = "地址：台灣台中市中區民族路67號";
+			shopTel.innerHTML = "電話：04-6666-5555";
+			shopOpen.innerHTML = "營業時間：0900-2100";
+		  	infowindow.open(map,marker4);
+		});
+	
+		google.maps.event.addListener(marker5, 'click', function() {
+			if(haveUserLocation){
+		  		if (locations.length>1){ //如果location[]有兩個元素，丟掉最後一個(第一個是使用者位置)
+		  			locations.pop();
+		  		} 
+	  		}else{
+	  			if (locations.length>1){locations =[];}
+	  		}	//		getLocation();//get user location
+			locations.push(marker5); //push marker in locations[1];
+	        DrawRoutePath();//root organize
+	//show shop info
+	        shopAddress.innerHTML = "地址：台灣台中市南屯區建功路50號";
+			shopTel.innerHTML = "電話：04-6666-3333";
+			shopOpen.innerHTML = "營業時間：0900-2100";
+			infowindow.open(map,marker5);
+		});
+			
+	//get user geolocation
+		//  var startNorth;
+		// 	var startEast;
+		//  1'st to last  (緯度Latitude(startNorth),經度Longitude(startEast))
+	//	var hint = document.getElementById("mapInfo"); //display mapInfo
+		
+	//	function getLocation() {
+	//	    if (navigator.geolocation) {
+	//	        navigator.geolocation.getCurrentPosition(showPosition, showError);
+	//	    } else {
+	//	    	hint.innerHTML = "Geolocation is not supported by this browser.";
+	//	    }
+	//	}
+	//
+	//	function showPosition(position) {
+	//		// if (locations.length>1){locations =[];} //clear array when triple click
+	//		startNorth = position.coords.latitude;
+	//		startEast = position.coords.longitude;
+	//		nowPosition = new google.maps.LatLng(startNorth, startEast);
+	////		locations.push(nowPosition);//put user position in [0]
+	//		hint.innerHTML = "現在位置：("+startNorth+","+startEast+")";		
+	//	}
+	//
+	//	function showError(error) {
+	//	    switch(error.code) {
+	//	        case error.PERMISSION_DENIED:
+	//	            x.innerHTML = "User denied the request for Geolocation."
+	//	            break;
+	//	        case error.POSITION_UNAVAILABLE:
+	//	            x.innerHTML = "Location information is unavailable."
+	//	            break;
+	//	        case error.TIMEOUT:
+	//	            x.innerHTML = "The request to get user location timed out."
+	//	            break;
+	//	        case error.UNKNOWN_ERROR:
+	//	            x.innerHTML = "An unknown error occurred."
+	//	            break;
+	//	    }
+	//	}
+	    
+	    //root path service
+	    var directionsService = new google.maps.DirectionsService();
+	    var directionsDisplay = new google.maps.DirectionsRenderer();
+	    directionsDisplay.setMap(map);
+	    
+	    function DrawRoutePath() {
+//	        if (locations.length < 2)
+//	            return;
+	        var start = locations[0].position;
+	        var end = locations[1].position
+	        var request = {
+	            origin : start,
+	            destination : end,
+	            travelMode : google.maps.TravelMode.DRIVING
+	        };
+	        directionsService.route(request, function(response, status) {
+	            if (status == google.maps.DirectionsStatus.OK) {
+	                directionsDisplay.setDirections(response);
+	            }
+	        });
+	    }  // end of DrawRoutePath()
+		}
+});
