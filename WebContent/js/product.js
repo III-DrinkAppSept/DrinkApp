fae74111071a36b98ceca9f5b020bcb2d6eae7c3
Hsuhulk <hsuchehao97@gmail.com>
2016-09-19 14:26:34 +0800

    0919_product_

--- WebContent/js/product.js
+++ WebContent/js/product.js
@@ -268,18 +268,23 @@ $("#joinOrder").click(function() {
         // console.log(order_product);
     }
 });
-
+//會員，暫時用01
 var isMember = false;
 var memberNum = '01';
 
 
 $(".addOrder").click(function() {
-
-    var category = parseInt(this.id.substr(13, 2));
-    var product = parseInt(this.id.substr(16, 2));
-    // console.log(menuDataGrop[category][product].name);
-    order_item_info = menuDataGrop[category][product];
-    // console.log(order_item_info);
+	if(loginAccount == ""){
+//		$.mobile.changePage("#member");
+		$('.addOrder').attr("href", "#popupRedirect"); 
+	}else{
+		$('.addOrder').attr("href", "#addPopup"); 
+	    var category = parseInt(this.id.substr(13, 2));
+	    var product = parseInt(this.id.substr(16, 2));
+	    // console.log(menuDataGrop[category][product].name);
+	    order_item_info = menuDataGrop[category][product];
+	    // console.log(order_item_info);
+	}
 });
 
 
@@ -287,6 +292,8 @@ $(".addOrder").click(function() {
 
 //<div data-role="collapsible"  data-collapsed-icon = "carat-d" data-expanded-icon = "carat-u" data-iconpos = "right">
 var ID = 0; //order_item的記數 每add成功一次+1
+var total_price = 0;
+
 function createOrderList(order_product) {
     var name = order_product.name;
     var capacity = order_product.capacity;
@@ -296,21 +303,59 @@ function createOrderList(order_product) {
     var price = order_product.price;
 
     var $h3 = $('<h3>');
+
+    var $dh1 = $('<div>');
+    var $dh2 = $('<div>');  
+    var $dh3 = $('<div>'); 
+
+    var $dhX = $('<div>');
+    $dhX.attr("id", "order_title_" + ID);
+    $dhX.addClass('ui-grid-b'); 
+
+    $dh1.addClass('ui-block-a');
+    $dh2.addClass('ui-block-b');  
+    $dh3.addClass('ui-block-c'); 
+
+    $dh1.append(name);
+    $dh2.append(quantity + '杯');
+    $dh3.append('共' + quantity * price + '元');
+    $dhX.append($dh1).append($dh2).append($dh3);
+    $h3.append($dhX);
+
+    // $h3.append(name + ' x ' + quantity + '共' + quantity * price + '元');
+
+    var $divX = $('<div>');
+    $divX.attr("id", "order_content_" + ID);
+    $divX.addClass('ui-grid-b');
     var $div1 = $('<div>');
     var $div2 = $('<div>');
 
-    $div1.addClass('ui-block-b');
-    $div2.addClass('ui-block-c');
-
-    $h3.append(name + ' x ' + quantity + '共' + price + '元');
+    $div1.addClass('ui-block-a');
+    $div2.addClass('ui-block-b');
 
     var $p1 = $('<p>');
     var $p2 = $('<p>');
-    $p1.append(capacity + ' ' + sugar + ' ' + ice + '  x ' + quantity + '共' + quantity * price + '元');
+
+    $p1.append(capacity + '&nbsp;&nbsp;' + sugar + '&nbsp;&nbsp;' + ice);
+    $p2.append('&nbsp;&nbsp;&nbsp;' + quantity + '杯&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + quantity * price + '元');
     $div1.append($p1);
+    $div2.append($p2);
+    var $div3 = $('<div>');
+    $div3.addClass('ui-block-c');
+    //data-rel="popup" data-position-to="window" data-transition="pop"
+    var $btn = $('<a>');
+    $btn.attr("href", "#popupDialog");
+    $btn.attr("data-rel", "popup");
+    $btn.attr("data-position-to", "window");
+    $btn.attr("data-transition", "pop");
+    $btn.attr("id", "btn_del_" + ID);
+    $btn.text("刪除訂單");
+    $btn.addClass("btnDelete ui-btn ui-corner-all ui-shadow ui-icon-delete ui-btn-icon-left ui-mini ui-btn-c");
+    $div3.append($btn);
 
+    $divX.append($div1).append($div2).append($div3);
 
-    var $orderList = $('<div>');    
+    var $orderList = $('<div>');
     $orderList.attr("data-role", "collapsible");
     $orderList.attr("data-collapsed-icon", "carat-d");
     $orderList.attr("data-expanded-icon", "carat-u");
@@ -320,101 +365,260 @@ function createOrderList(order_product) {
     $orderList.addClass('orderList');
 
     $orderList.append($h3);
-    $orderList.append($p1);
-
+    // $orderList.append($p1);
+    $orderList.append($divX);
     $orderList.collapsible();
 
     $('#orderContent').append($orderList);
     // console.log($('#orderContent').html());
     $("#set0").after($orderList);
+    total_price += quantity * price;
+    // console.log(total_price);
+    set_total();
+    detect_dispatch();
+    // console.log(order_items);
 }
 
-//將相同品名的collapse合起來<---待做
+//將相同品名的collapse合起來
+//需考慮已刪除的id
+
+var merged_Nums = [];//用來紀錄已被合併訂單號碼
 function mergeOrderList() {
-    // for(var i = 0; i < ID; i++){
-    //    var str = $('#order_item_' + i + ' p').html();
-    //    console.log(str);
-    // }
     for (var i = 0; i < order_items.length; i++) {
-        for (var j = 0; j < order_items.length; j++) {
-            if (i == j) continue;
-            if (order_items[i].name == order_items[j].name) {
-                //將名稱重複的取出來跟上一筆合併，並刪除
-                var $px = $('<p>');
-                var str = $('#order_item_' + i + ' p').html();
-                $px.append(str);
-                $('#order_item_' + j + ' .ui-collapsible-content').append($px);
-                console.log($px);
+        for (var j = 0; j < i; j++) {
+            if (check_deleted(j) && check_merged(j)) {
+                if (order_items[i].name == order_items[j].name) {
+                    //將名稱重複的取出來跟上一筆合併，並刪除
+                    var del = $('#order_content_' + i);
+
+                    $('#order_item_' + j + ' .ui-collapsible-content').append(del);
+                    $('#order_item_' + j).collapsible();
+                    var qty1 = parseInt($('#order_title_' + i + ' div:nth-child(2)').text());
+                    var rd1 = $('#order_title_' + i + ' div:nth-child(3)').text();
+                    var price1 = parseInt(rd1.substr(1,rd1.indexOf('元')-1));
+                    var qty2 = parseInt($('#order_title_' + j + ' div:nth-child(2)').text());
+                    var rd2 = $('#order_title_' + j + ' div:nth-child(3)').text();
+                    var price2 = parseInt(rd2.substr(1,rd2.indexOf('元')-1));
+
+                    $('#order_item_' + i).remove();
+                    merged_Nums.push(i);
+                    set_list_header(order_items[j].name,qty1+qty2,price1+price2,j);
+                    // console.log('qty1 : ' + qty1 + ',qty2 : ' + qty2);
+                    // console.log('price1 : ' + price1 + ',price2 : ' + price2);
+                }
             }
         }
     }
 }
 
+//collapse標題列
+function set_list_header(name,quantity,price,num){
 
-$('#btnConfirm').click(function() {
-    // mergeOrderList();
+    var $dh1 = $('<div>');
+    var $dh2 = $('<div>');  
+    var $dh3 = $('<div>'); 
+
+    var $dhX = $("#order_title_" + num);
+    $dhX.empty();
+    $dhX.addClass('ui-grid-b'); 
+
+    $dh1.addClass('ui-block-a');
+    $dh2.addClass('ui-block-b');  
+    $dh3.addClass('ui-block-c'); 
+
+    $dh1.append(name);
+    $dh2.append(quantity + '杯');
+    $dh3.append('共' + price + '元');
+    $dhX.append($dh1).append($dh2).append($dh3);
+}
+
+function get_root (delnum) {
+    for(var i = 0; i < order_items.length ; i++){
+        if($('#order_title_' + i).length){
+            if($('#order_title_' + i + ' div:first').text() == order_items[delnum].name){
+                return i ; 
+            }
+        }
+    }
+}
+
+var delete_num; //記錄當前項目的ID(createOrderList方法上的)
+var delete_nums = [];
+//按下刪除鍵後跳出刪除視窗
+$(document).on("click", ".btnDelete", function() {
+    delete_num = this.id.substr(8);
+    $(".delete-confirm h3").text($("#order_content_" + delete_num + " p").text());
 });
 
+
+$("#delete_order").click(function() {
+    var dqty = order_items[delete_num].quantity;
+    var dprice = order_items[delete_num].price * dqty;
+    $("#order_content_" + delete_num).remove();
+    total_price -= order_items[delete_num].quantity * order_items[delete_num].price;
+    set_total();
+    detect_dispatch();
+
+    delete_nums.push(delete_num);
+    for(var i = 0; i < delete_nums.length; i++){
+        if ($("#order_item_" + delete_nums[i]).has('p').length < 1) {
+            $("#order_item_" + delete_nums[i]).remove();
+        }
+    }
+    var root_num = get_root(delete_num);
+
+    var gqty = parseInt($('#order_title_' + root_num + ' div:nth-child(2)').text());
+    var rd3 = $('#order_title_' + root_num + ' div:nth-child(3)').text();
+    var gprice = parseInt(rd3.substr(1,rd3.indexOf('元')-1));
+
+    console.log(gprice + ":" + dprice + ":" + (gprice-dprice));
+    if(root_num > -1){
+        set_list_header(order_items[root_num].name, gqty - dqty,gprice-dprice,root_num);
+    }
+});
+
+
+$('#btnConfirm').click(function() {
+	if(loginAccount == ""){
+//		alert("請登入會員");
+		$('#btnConfirm').attr("href", "#popupRedirect"); 
+		$('#btnConfirm').attr("data-rel", "popup"); 
+		$('#btnConfirm').attr("data-transition", "slide");
+	}else{
+
+		 mergeOrderList();
+		$('#btnConfirm').removeAttr("data-rel"); 
+		$('#btnConfirm').removeAttr("data-transition");
+		 $('#btnConfirm').attr("href","#orderPage");
+	}
+});
+$('#redirect_confirm').click(function() {
+	$.mobile.changePage("#member");
+});
+//返回產品頁後將訂單頁重製
+$('#btnOrderBack').click(function() {
+    $('#orderContent [data-role="collapsible"]').collapsible("collapse");
+});
+//清空列表
+$('#btnClearOrder').click(function() {
+    $('#orderContent [data-role="collapsible"]').remove();
+    order_items = [];
+    delete_nums = [];
+    total_price = 0;
+    delete_num = 0;
+    ID = 0;
+    // console.log(order_items);
+    set_total();
+    detect_dispatch();
+
+});
+
+//外送初始值
+$("input[type='radio']").checkboxradio();
+$('#group_dispatch input[type="radio"]').checkboxradio('disable');
+$('#group_dispatch input[type="radio"]').checkboxradio('refresh');
+$('#text_addr').prop('disabled', true);
+//金額滿300，可以外送
+function detect_dispatch() {
+    if (total_price < 300) {
+        $('#group_dispatch input[type="radio"]').filter('[value=on]').prop('checked', true);
+        $('#text_addr').val('');
+        $('#group_dispatch input[type="radio"]').checkboxradio('disable');
+        $('#group_dispatch input[type="radio"]').checkboxradio('refresh');
+    } else {
+        $('#group_dispatch input[type="radio"]').checkboxradio('enable');
+        $('#group_dispatch input[type="radio"]').checkboxradio('refresh');
+    }
+}
+$('#group_dispatch input[type="radio"]').click(function (event) {
+    event.preventDefault();
+    if($('label[for="' + this.id + '"]').text() == "外送"){
+        $('#text_addr').val(orderAdress);
+        $('#text_addr').textinput('enable');
+        console.log(orderAdress);
+    }else {
+        $('#text_addr').prop('disabled', true);
+        $('#text_addr').val('');
+    }
+});
+
+function set_total() {
+    $("#total_price").html("總計 :  " + total_price + " 元");
+}
+
+//檢查此ID是否已刪除
+function check_deleted(val) {
+    for (var i = 0; i < delete_nums.length; i++) {
+        if (val == delete_nums[i]) return false;
+    }
+    return true;
+}
+//檢查此ID是否已刪除
+function check_merged(val) {
+    for (var i = 0; i < merged_Nums.length; i++) {
+        if (val == merged_Nums[i]) return false;
+    }
+    return true;
+}
 //ajax
-var url_orderlist = "http://127.0.0.1:8080/app/SendOrderList?";
-var url_orderitem = "http://127.0.0.1:8080/app/SendOrderItem?";
+var url_orderlist = "http://127.0.0.1:8080/DrinkApp/SendOrderList?";
+var url_orderitem = "http://127.0.0.1:8080/DrinkApp/SendOrderItem?";
 
 
-function SendOrderList(orderNum, customerId, orderDate, status, dispatch,note) {
-	var url_true = encodeURI(url_orderlist + "orderNum=" + orderNum + "&customerId=" + customerId + "&orderDate=" + orderDate + "&status=新訂單" + "&dispatch=" + dispatch + "&note=" + note);
+function SendOrderList(orderNum, customerId, orderDate, status, dispatch, note) {
+    var url_true = encodeURI(url_orderlist + "orderNum=" + orderNum + "&customerId=" + customerId + "&orderDate=" + orderDate + "&status=新訂單" + "&dispatch=" + dispatch + "&note=" + note);
     $.ajax({
         async: false,
         type: 'POST',
         url: url_true,
         success: function(data) {
-        	alert('orderlist insert');
+//            alert('orderlist insert');
         },
-    	error: function (){
-    		alert('orderlist false');
-    	},
-        contentType:'application/x-www-form-urlencoded; charset=UTF-8'
+        error: function() {
+//            alert('orderlist false');
+        },
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
     });
 }
 
-function SendOrderItem(orderNum,order_product) {
+function SendOrderItem(orderNum, order_product) {
     var name;
-    if(order_product.capacity == "中杯")
-        name = order_product.name+'(M)';
+    if (order_product.capacity == "中杯")
+        name = order_product.name + '(M)';
     else
-        name = order_product.name+'(L)';
+        name = order_product.name + '(L)';
     var note = order_product.sugar + order_product.ice;
-    var url_true = encodeURI(url_orderitem + 'orderNum='+ orderNum + '&productName=' + name + '&qty=' + order_product.quantity + '&note=' + note);
+    var url_true = encodeURI(url_orderitem + 'orderNum=' + orderNum + '&productName=' + name + '&qty=' + order_product.quantity + '&note=' + note);
     $.ajax({
         async: false,
         type: 'POST',
         url: url_true,
         success: function(data) {
-        	alert('orderitem insert');
+//            alert('orderitem insert');
         },
-    	error: function (){
-    		alert('orderitem false');
-    	},
-    	contentType:'application/x-www-form-urlencoded; charset=UTF-8'
-    	
+        error: function() {
+//            alert('orderitem false');
+        },
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
+
     });
 }
 
 $('#btnCheckPay').click(function() {
-    // mergeOrderList();
     var mydate = new Date();
     var month = mydate.getMonth() + 1;
     if (month < 10) month = '0' + month;
     //訂單編號
-    var listNum = memberNum + mydate.getFullYear() + month + mydate.getDate() + mydate.getHours() + mydate.getMinutes() + mydate.getSeconds();
+    var listNum = loginAccount + mydate.getFullYear() + month + mydate.getDate() + mydate.getHours() + mydate.getMinutes() + mydate.getSeconds();
     var label_dispatch = $('label[for="' + $("#group_dispatch input:checked").attr('id') + '"]').text();
-    var note = '';
-    var orderDate = mydate.getFullYear() + "/" + month + "/" 
-    				+ mydate.getDate() + " " + mydate.getHours()+ ":" 
-    				+ mydate.getMinutes()+ ":" + mydate.getSeconds();
-    SendOrderList(listNum, memberNum, orderDate,'新訂單',label_dispatch,note);
-    for(var i = 0 ; i < order_items.length; i++){
-        SendOrderItem(listNum,order_items[i]);
+    var note = '';//住址
+    var orderDate = mydate.getFullYear() + "/" + month + "/" + mydate.getDate() + " " + mydate.getHours() + ":" + mydate.getMinutes() + ":" + mydate.getSeconds();
+    SendOrderList(listNum, loginAccount, orderDate, '新訂單', label_dispatch, note);
+    for (var i = 0; i < order_items.length; i++) { 
+        if(check_deleted(i)){ //須扣除delete_nums中的
+            SendOrderItem(listNum, order_items[i]);
+        }
     }
-     console.log('OK');
+    console.log('OK');
 
 });
